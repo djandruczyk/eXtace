1961b9aac5b7f1e40e428886ad5af6cc5898c13e
extace <extace>
2003-05-08 03:21:50 +0000

    Removed stars.h (old cruft)

--- src/stars.c
+++ src/stars.c
@@ -24,6 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
+#include <protos.h>
 
 #define STARS_NUM 640
 
